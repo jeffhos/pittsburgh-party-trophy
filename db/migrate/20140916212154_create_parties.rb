fa3aa39a4c837be75da0fb7ab91b9723678ee1d3
Jeff Hoskinson <hoskinsonj@upmc.edu>
2014-09-22 16:40:10 -0400

    Implement basic home and party creation pages

--- db/migrate/20140916212154_create_parties.rb
+++ db/migrate/20140916212154_create_parties.rb
@@ -3,7 +3,7 @@ class CreateParties < ActiveRecord::Migration
     create_table :parties do |t|
       t.string :name
       t.text :description
-      t.boolean :approved
+      t.boolean :approved, default: false
       t.date :happened_on
 
       t.timestamps
