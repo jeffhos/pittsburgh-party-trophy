c420c8ab02dd8a382482658b9e9216ab9e848782
Jeff Hoskinson <jeffhos@eclecsia.org>
2014-10-26 21:48:46 -0400

    Generate thumbnails for photos after the first

--- app/models/party.rb
+++ app/models/party.rb
@@ -2,9 +2,9 @@ class Party < ActiveRecord::Base
   attr_accessible :happened_on, :approved, :description, :name, :image1, :image2, :image3
   has_attached_file :image1, :styles => { :thumb => "200x200>" }
   validates_attachment_content_type :image1, :content_type => /\Aimage/
-  has_attached_file :image2
+  has_attached_file :image2, :styles => { :thumb => "200x200>" }
   validates_attachment_content_type :image2, :content_type => /\Aimage/
-  has_attached_file :image3
+  has_attached_file :image3, :styles => { :thumb => "200x200>" }
   validates_attachment_content_type :image3, :content_type => /\Aimage/
   validates_presence_of :name
   validates :happened_on, date: { before_or_equal_to: Proc.new { Date.today } }
