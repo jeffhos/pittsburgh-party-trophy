f6f1dfe1beb18e997620691e10f71ef99711f709
Jeff Hoskinson <github@eclecsia.org>
2014-10-30 18:16:55 -0400

    Convert everything over to Slim templates; other clean-up

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,7 +1,7 @@
 class PartiesController < ApplicationController
 
   def index
-    @parties = Party.where('approved = ?', true).order('happened_on DESC').page(params[:page]).per(5)
+    @parties = Party.where(:approved => true).order('happened_on DESC').page(params[:page]).per(5)
   end
 
   def show
@@ -16,9 +16,9 @@ class PartiesController < ApplicationController
     @party = Party.new(params[:party])
 
     if @party.save
-      redirect_to controller: :home, action: :thanks
+      redirect_to :controller => :home, :action => :thanks
     else
-      render action: "new"
+      render :action => 'new'
     end
   end
 
